966bae6cdb0e98545f3f406b931fc69f787bd418
Syohei YOSHIDA <syohex@gmail.com>
2013-04-27 17:56:12 +0900

    Add URL

--- open-github.el
+++ open-github.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2013 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
-;; URL:
+;; URL: https://github.com/syohex/emacs-open-github
 ;; Version: 0.01
 ;; Package-Requires: ((helm "1.0") (gh "1.0"))
 
