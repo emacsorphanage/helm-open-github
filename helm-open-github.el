ce0da7273093436f6da475ab5f8dcb9e0f055691
Syohei YOSHIDA <syohex@gmail.com>
2016-12-03 22:54:41 +0900

    Require Emacs 24.4 or higher versions

--- helm-open-github.el
+++ helm-open-github.el
@@ -1,11 +1,11 @@
 ;;; helm-open-github.el --- Utilities of Opening Github Page -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2015 by Syohei YOSHIDA
+;; Copyright (C) 2016 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-open-github
 ;; Version: 0.14
-;; Package-Requires: ((helm-core "1.7.7") (gh "0.8.2") (cl-lib "0.5"))
+;; Package-Requires: ((emacs "24.4") (helm-core "1.7.7") (gh "0.8.2"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
