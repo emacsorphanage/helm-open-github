553f3ab0fe0a028015e9b6cb7c35fb139ec222fc
Syohei YOSHIDA <syohex@gmail.com>
2016-12-03 23:04:40 +0900

    Bump up version and update Changes

--- helm-open-github.el
+++ helm-open-github.el
@@ -4,7 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-open-github
-;; Version: 0.14
+;; Version: 0.15
 ;; Package-Requires: ((emacs "24.4") (helm-core "1.7.7") (gh "0.8.2"))
 
 ;; This program is free software; you can redistribute it and/or modify
